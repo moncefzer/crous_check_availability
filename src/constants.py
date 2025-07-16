f8fa378a97f56e25e320ca182abf0697be37e9a3
moncefzer <moncefzermache@gmail.com>
2025-07-16 10:18:05 +0200

    add aubiere

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+LOCATIONS = { "clermont-ferrand": [{'lon': 3.0532561, 'lat': 45.8183838}, {'lon': 3.1721761, 'lat': 45.7556941}], 
+            "aubiere": [{'lon': 3.1002125, 'lat': 45.7643518}, {'lon': 3.1541077, 'lat': 45.7396976}] }
+
+
+CHECK_INTERVAL = 60 * 3        # Every 4min
+HEARTBEAT_INTERVAL = 60 * 60 * 5 # Every 5h
